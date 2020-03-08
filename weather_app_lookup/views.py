0f68fd514217c13ef1def9dcb89f67ae434f4bfd
Demetrios <you@youraddress.com>
2020-03-08 19:29:03 +0000

    Added

--- weather_app_lookup/views.py
+++ weather_app_lookup/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+
 # This is a comment to test git add
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    # api_requests = requests.get('http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=7&API_KEY=77EB95EF-92E8-4117-9C1F-AAD1B6A33C1A')
+    api_request = requests.get('https://data.police.uk/api/stops-no-location?force=cleveland&date=2017-01')
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error.... Try agin!"
+
+    return render(request, 'home.html', {'api': api})
 
 def about(request):
     return render(request, 'about.html', {})
